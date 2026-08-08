f1370c28ab41a8b3084d488830882d1fc04e177f
Ashmit Rawat <ashmitrawat740@gmail.com>
2026-08-09 02:13:39 +0530

    feat: complete question on vowel consonant count

---
+++ Week2/q2_csvstudentrecord.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+public class q2_csvstudentrecord {
+    public static void parsestudentrecord(String csvline) {
+        String data[] = csvLine.split(",");
+        if(data.length < 3){
+            System.out.println("Invalide Record");
+        }
+        String name = data[0];
+        String rollNo = data[1];
+        String department  = data[2];
+        System.out.println("Name:" + name+ "| Roll No:" +rollNo+ "| Department:" + department);
+    }
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        Strung csvLine = sc.nextLine;
+        parsestudentrecord(csvline);
+    }
+}
---
+++ Week2/q5_bank.java
@@ -0,0 +1,52 @@
+import java.util.Scanner;
+
+public class q5_bank {
+
+    static String normalizeReference(String raw) {
+        raw = raw.trim();
+
+        String bank = raw.substring(0, 3).toUpperCase();
+
+        return bank + raw.substring(3);
+    }
+
+    static String validateAndFormat(String reference) {
+
+        if (reference.length() != 14) {
+            return "Invalid: wrong length";
+        }
+
+        String bank = reference.substring(0, 3);
+        String date = reference.substring(3, 9);
+        String seq = reference.substring(9, 14);
+
+        for (int i = 0; i < 3; i++) {
+            if (!Character.isLetter(bank.charAt(i))) {
+                return "Invalid: bank code must be 3 letters";
+            }
+        }
+
+        for (int i = 0; i < 11; i++) {
+            if (!Character.isDigit(reference.charAt(i + 3))) {
+                return "Invalid: remaining characters must be digits";
+            }
+        }
+
+        return "[" + bank + "] DATE: "
+                + date.substring(0, 2) + "/"
+                + date.substring(2, 4) + "/"
+                + date.substring(4, 6)
+                + " | SEQ: " + seq;
+    }
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        String raw = sc.nextLine();
+
+        String reference = normalizeReference(raw);
+
+        System.out.println(validateAndFormat(reference));
+    }
+}
