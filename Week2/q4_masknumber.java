a30476b3d51be133da7a55e6eddfc086b6dd9cf3
Ashmit Rawat <ashmitrawat740@gmail.com>
2026-08-09 02:20:01 +0530

    feat: complete question on masking phone numbers

--- Week2/q4_masknumber.java
+++ Week2/q4_masknumber.java
@@ -6,7 +6,7 @@ public class q4_masknumber {
         }
         for (int i = 0; i < phone.length(); i++) {
             if (!Character.isDigit(phone.charAt(i))) {
-                return "Invalid phone number";
+                return "Invalid phone number ";
             }
         }
         String lastFour = phone.substring(6);
